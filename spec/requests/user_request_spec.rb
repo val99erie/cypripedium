182e24cfa65e48ffc2e80a8b960d4157b2540c64
Rachel M. Kadel <rachelkg@gmail.com>
2020-08-17 16:04:57 -0400

    Update sorting test for format changes

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe "redirect from new user form", :type => :request do
+RSpec.describe "redirect from new user form", type: :request do
   it "redirects to the front page" do
     get "/users/sign_up"
     response.should redirect_to("/?locale=en")
--- spec/system/collection_item_ordering_spec.rb
+++ spec/system/collection_item_ordering_spec.rb
@@ -40,16 +40,16 @@ RSpec.describe 'When displaying a collection, sort by issue number', type: :syst
   context 'order the items in a collection' do
     it 'shows the works sorted by issue number descending when the page first loads' do
       visit Hyrax::Engine.routes.url_helpers.collection_path(collection.id)
-      expect(page.find(:xpath, '/HTML/BODY[1]/DIV[3]/DIV[1]/DIV[7]/TABLE[1]/TBODY[1]/TR[1]/TD[3]').text). to eq "5"
-      expect(page.find(:xpath, '/HTML/BODY[1]/DIV[3]/DIV[1]/DIV[7]/TABLE[1]/TBODY[1]/TR[5]/TD[3]').text). to eq "1"
+      expect(page.find(:xpath, '/html/body/div[3]/div[1]/div/div[7]/table/tbody/tr[1]/td[3]').text). to eq "5"
+      expect(page.find(:xpath, '/html/body/div[3]/div[1]/div/div[7]/table/tbody/tr[5]/td[3]').text). to eq "1"
     end
 
     it 'can reorder by title' do
       visit Hyrax::Engine.routes.url_helpers.collection_path(collection.id)
       page.find(:css, '#sort').find(:xpath, 'option[6]').select_option
       page.find_all(:xpath, '//button')[2].click
-      expect(page.find(:xpath, '/HTML/BODY[1]/DIV[3]/DIV[1]/DIV[7]/TABLE[1]/TBODY[1]/TR[1]/TD[3]').text). to eq "1"
-      expect(page.find(:xpath, '/HTML/BODY[1]/DIV[3]/DIV[1]/DIV[7]/TABLE[1]/TBODY[1]/TR[5]/TD[3]').text). to eq "5"
+      expect(page.find(:xpath, '/html/body/div[3]/div[1]/div/div[7]/table/tbody/tr[1]/td[3]').text). to eq "1"
+      expect(page.find(:xpath, '/html/body/div[3]/div[1]/div/div[7]/table/tbody/tr[5]/td[3]').text). to eq "5"
     end
   end
 end
